93c76cfafbf2de864b8c8eacf8efb3d47e99711f
abigailarmijohernandez <abi1521@hotmail.com>
2021-09-05 21:07:17 -0500

    Initial commit basic

---
+++ sauceDemo/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace sauceDemo
+{
+    public class Constants
+    {
+        public static string INVENTORY_PAGE = "inventory.html";
+        public static string BASE_ADDRESS = "https://www.saucedemo.com";
+
+    }
+}
