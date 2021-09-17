3628514059dd85d14a25bb2b2c17c9b4a4ff5f75
abigailarmijohernandez <abi1521@hotmail.com>
2021-09-17 17:08:14 -0500

    Initial Login Test Cases

---
+++ sauceDemo/Base/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace sauceDemo
+{
+    /// <summary>
+    /// Basse Page with common functions to all pages
+    /// </summary>
+    public class BasePage
+    {
+        protected IPage Page;
+        private string burgerMenuId = "#react-burger-menu-btn";
+        private string logoutMenuItem = "#logout_sidebar_link";
+
+        public BasePage(IPage page)
+        {
+            this.Page = page;
+        }
+
+        public async Task ClickMenu() => await Page.ClickAsync(burgerMenuId);
+
+        public async Task Logout()
+        {
+            await ClickMenu();
+            await Page.ClickAsync(logoutMenuItem);
+        }
+    }
+}
---
+++ sauceDemo/Base/Initialize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sauceDemo
+{
+    [TestClass]
+    public class Initialize
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            
+ 
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            
+        }
+    }
+}
