4c9c438adf60c52f523ef98c4922e14ae3473fcd
abigailarmijohernandez <abi1521@hotmail.com>
2021-10-21 21:54:51 -0500

    Fix parameter

--- sauceDemo/Base/Initialize.cs
+++ sauceDemo/Base/Initialize.cs
@@ -9,12 +9,12 @@ namespace sauceDemo
     {
 
         public static IPage Page;
-        public static string BaseAddress = "https://www.saucedemo.com/";
+        public static string BaseAddress;
 
         [OneTimeSetUp]
         public static void  Setup()
         {
-           // BaseAddress = Environment.GetEnvironmentVariable(Constants.BASE_ADDRESS);
+           BaseAddress = Environment.GetEnvironmentVariable(Constants.BASE_ADDRESS);
         }
 
         [OneTimeTearDown]
